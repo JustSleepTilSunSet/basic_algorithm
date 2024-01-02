44390fe31626f2071c1dfa82469e695229bc98d6
JustSleepTilSunSet <cloverchen.042@gmail.com>
2024-01-02 22:11:48 +0800

    Implement quicksort.

--- QuickSort/index.c
+++ QuickSort/index.c
@@ -4,12 +4,11 @@
 int testcase[] = {3, 2, 5, 0, 1, 8, 7, 6, 9, 4}; // confirm
 int testcaseLen = sizeof(testcase) / sizeof(int);
 void swapByIdx(int, int); // confirm
-void QuickSort(int, int, int);
+void QuickSort(int, int);
 void toShow(); // confirm
 void main()
 {
-    // printf("%d ",testcase[8]);
-    QuickSort(0, 0, testcaseLen - 1);
+    QuickSort(-1, testcaseLen - 1);
     toShow();
     system("pause");
 }
@@ -20,30 +19,23 @@ void swapByIdx(int idx1, int idx2)
     testcase[idx1] = testcase[idx2];
     testcase[idx2] = tp;
 }
-void QuickSort(int currentIdx, int swapMarker, int pv)
+void QuickSort(int left, int right)
 {
-    if (currentIdx == testcaseLen)
-    {
+    if (left >= right)
         return;
-    }
-    if (testcase[currentIdx] > testcase[pv])
+    int swapMarker = left;
+    int currentIndex = left;
+    for (currentIndex = left + 1; currentIndex <= right - 1; currentIndex++)
     {
-        return QuickSort(currentIdx + 1, swapMarker, pv);
-    }
-    else if (testcase[currentIdx] <= testcase[pv])
-    {
-        swapMarker++;
-        if (currentIdx > swapMarker)
+        if (testcase[currentIndex] <= testcase[right])
         {
-            swapByIdx(currentIdx, swapMarker);
-            return QuickSort(currentIdx + 1, swapMarker, pv);
-        }
-        else
-        {
-            return QuickSort(currentIdx + 1, currentIdx, pv);
+            swapMarker++;
+            swapByIdx(currentIndex, swapMarker);
         }
     }
-    return;
+    swapByIdx(swapMarker + 1, right);
+    QuickSort(left, swapMarker);
+    QuickSort(swapMarker + 1, right);
 }
 
 void toShow()
